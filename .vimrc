f58df4b870b475efe9546699277c5ce4fb2d9e8f
Edinei Cavalcanti <edinei.esc@gmail.com>
2014-11-01 15:57:57 -0200

    Init

---
+++ .vimrc
@@ -0,0 +1,11 @@
+syntax on
+filetype indent plugin on
+
+"set tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+set et sw=2 sts=2 tw=80 ts=8 kp=man
+au FileType c set noet sw=8 sts=0 ts=8
+au FileType go set noet sw=4 sts=0 ts=4
+au FileType python set sw=4 sts=4 tw=79 kp=pydoc
+au FileType ruby set kp=ri
+
---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+DOTFILES_FOLDER=$HOME/dotfiles
+DB_FOLDER=$HOME/Dropbox
+
+if [ -d $DB_FOLDER ]; then
+  [ ! -L $HOME/.zsh_history ] && ln -s $DB_FOLDER/history/zsh_history $HOME/.zsh_history
+fi
+
+## link to $HOME
+ln -s $DOTFILES_FOLDER/.gitconfig $HOME
+ln -s $DOTFILES_FOLDER/.gitignore_global $HOME
+ln -s $DOTFILES_FOLDER/.vimrc $HOME
+ln -s $DOTFILES_FOLDER/.xinitrc $HOME
+ln -s $DOTFILES_FOLDER/.zshrc $HOME

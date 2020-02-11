a527fd07ffd1067f1022ad9fb827435f8a443c4e
Edinei Cavalcanti <edinei.esc@gmail.com>
2020-02-11 18:37:10 -0300

    Remove virtualbox

--- windows/common_apps.ps1
+++ windows/common_apps.ps1
@@ -14,101 +14,99 @@ cinst superbenchmarker -y
 
 cinst autohotkey -y
 
- # Browsers
- cinst googlechrome -y
- cinst httrack -y
+# Browsers
+cinst googlechrome -y
+cinst httrack -y
 
- # Chrome exetensions
- #https://chrome.google.com/webstore/detail/json-viewer/gbmdgpbipfallnflgajpaliibnhdgobh
- #https://chrome.google.com/webstore/detail/google-optimize/bhdplaindhdkiflmbfbciehdccfhegci
- #https://chrome.google.com/webstore/detail/wakatime/jnbbnacmeggbgdjgaoojpmhdlkkpblgi
- #https://chrome.google.com/webstore/detail/web-timer/ggnjbdfgigejghknieofeahaknkjafim?utm_source=chrome-app-launcher-info-dialog
- #https://chrome.google.com/webstore/detail/tamper-chrome-extension/hifhgpdkfodlpnlmlnmhchnkepplebkb/
- #http://unpaywall.org/
- #https://chrome.google.com/webstore/detail/the-great-suspender/klbibkeccnjlkjkiokjodocebajanakg?hl=en
- ## Trello
- #https://chrome.google.com/webstore/detail/kanban-wip-for-trello/oekefjibcnongmmmmkdiofgeppfkmdii
- #https://chrome.google.com/webstore/detail/cardcounter-for-trello-wi/jbdmgabkegidhagemkfdogmplgelaoab
- #https://chrome.google.com/webstore/detail/chrome-remote-desktop/inomeogfingihgjfjlpeplalcfajhgai
+# Chrome exetensions
+#https://chrome.google.com/webstore/detail/json-viewer/gbmdgpbipfallnflgajpaliibnhdgobh
+#https://chrome.google.com/webstore/detail/google-optimize/bhdplaindhdkiflmbfbciehdccfhegci
+#https://chrome.google.com/webstore/detail/wakatime/jnbbnacmeggbgdjgaoojpmhdlkkpblgi
+#https://chrome.google.com/webstore/detail/web-timer/ggnjbdfgigejghknieofeahaknkjafim?utm_source=chrome-app-launcher-info-dialog
+#https://chrome.google.com/webstore/detail/tamper-chrome-extension/hifhgpdkfodlpnlmlnmhchnkepplebkb/
+#http://unpaywall.org/
+#https://chrome.google.com/webstore/detail/the-great-suspender/klbibkeccnjlkjkiokjodocebajanakg?hl=en
+## Trello
+#https://chrome.google.com/webstore/detail/kanban-wip-for-trello/oekefjibcnongmmmmkdiofgeppfkmdii
+#https://chrome.google.com/webstore/detail/cardcounter-for-trello-wi/jbdmgabkegidhagemkfdogmplgelaoab
+#https://chrome.google.com/webstore/detail/chrome-remote-desktop/inomeogfingihgjfjlpeplalcfajhgai
  
- cinst firefox-nightly -pre -y --ignore-checksums
+cinst firefox-nightly -pre -y --ignore-checksums
  
- # Firefox exetensions
- #https://addons.mozilla.org/en-US/firefox/addon/ff-tab-suspender/?src=search
+# Firefox exetensions
+#https://addons.mozilla.org/en-US/firefox/addon/ff-tab-suspender/?src=search
  
- # Security
- cinst veracrypt -y
+# Security
+cinst veracrypt -y
  
- cinst libreoffice-fresh -y
- cinst sumatrapdf.install -y
- cinst ultradefrag -y
+cinst libreoffice-fresh -y
+cinst sumatrapdf.install -y
+cinst ultradefrag -y
  
- #Win10 track
- cinst disablewintracking -y
- cinst shutup10 -y
+#Win10 track
+cinst disablewintracking -y
+cinst shutup10 -y
  
- cinst revo.uninstaller -y
- cinst dns-benchmark -y
- cinst sysinternals -y
+cinst revo.uninstaller -y
+cinst dns-benchmark -y
+cinst sysinternals -y
  
- cinst aida64-engineer -y
- cinst driverfusion -y
+cinst aida64-engineer -y
+cinst driverfusion -y
  
- # Disk and Files
- cinst crystaldiskmark -y
- cinst hdtune -y
- cinst renamer -y
- cinst teracopy -y
- cinst windirstat -y
- cinst lockhunter -y
+# Disk and Files
+cinst crystaldiskmark -y
+cinst hdtune -y
+cinst renamer -y
+cinst teracopy -y
+cinst windirstat -y
+cinst lockhunter -y
  
- # Network
- cinst microsoft-message-analyzer -y
- cinst wireshark -y
+# Network
+cinst microsoft-message-analyzer -y
+cinst wireshark -y
  
- cinst teamviewer -y
- cinst virtualbox -y
- cinst VirtualBox.ExtensionPack -y
+cinst teamviewer -y
  
- cinst yumi -y #Multiboot USB Creator
- 
- cinst dropbox -y
- cinst megasync -y
- 
- cinst filezilla -y
- #cinst winscp -y
- cinst 7zip -y
- 
- cinst keepass -y
- 
- cinst clementine -y
- cinst vlc -y
- ## cinst kodi -y
- 
- ## grammarly http://download-editor.grammarly.com/windows/GrammarlySetup.exe
- 
- cinst wcat -y #Web Capacity Analysis Tool
- cinst diskspd -y
- #cinst jmeter
- #cinst prime95
- cinst geogebra -y
- 
- cinst anki --version 2.0.52 -y # because helpful addons.
- # https://www.quora.com/What-are-the-most-useful-Anki-plugins
- 
- cinst xmind -y
- 
- # PE Explorer http://www.heaventools.com/overview.htm
- # IDA https://www.hex-rays.com/products/decompiler/index.shtml
- # https://github.com/fireeye/flare-ida/blob/master/python/flare/stackstrings.py
- 
- cinst youtube-dl -y
- 
- # Launcher
- cinst wox -y
- #cinst launchy -y
- #cinst everything -y
- 
- cinst firacode -y
- 
- # https://www.microsoft.com/pt-br/p/microsoft-whiteboard/9mspc6mp8fm4?activetab=pivot:overviewtab
+cinst yumi -y #Multiboot USB Creator
+
+cinst dropbox -y
+cinst megasync -y
+
+cinst filezilla -y
+#cinst winscp -y
+cinst 7zip -y
+
+cinst keepass -y
+
+cinst clementine -y
+cinst vlc -y
+## cinst kodi -y
+
+## grammarly http://download-editor.grammarly.com/windows/GrammarlySetup.exe
+
+cinst wcat -y #Web Capacity Analysis Tool
+cinst diskspd -y
+#cinst jmeter
+#cinst prime95
+cinst geogebra -y
+
+cinst anki --version 2.0.52 -y # because helpful addons.
+# https://www.quora.com/What-are-the-most-useful-Anki-plugins
+
+cinst xmind -y
+
+# PE Explorer http://www.heaventools.com/overview.htm
+# IDA https://www.hex-rays.com/products/decompiler/index.shtml
+# https://github.com/fireeye/flare-ida/blob/master/python/flare/stackstrings.py
+
+cinst youtube-dl -y
+
+# Launcher
+cinst wox -y
+#cinst launchy -y
+#cinst everything -y
+
+cinst firacode -y
+
+# https://www.microsoft.com/pt-br/p/microsoft-whiteboard/9mspc6mp8fm4?activetab=pivot:overviewtab

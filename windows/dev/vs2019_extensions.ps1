18ff1b23a877137b72e53d756d6967fc84372ae9
Edinei Cavalcanti <edinei.esc@gmail.com>
2020-10-29 10:54:35 -0300

    VS 2019: Add CodeMaintainibility

--- windows/dev/vs2019_extensions.ps1
+++ windows/dev/vs2019_extensions.ps1
@@ -3,5 +3,8 @@ if (Test-Path($ChocolateyProfile)) {
   Import-Module $ChocolateyProfile
 }
 
-# Code style and debbug
+# Code style and debug
 Install-ChocolateyVsixPackage -PackageName "Diagnostics.DiagnosticsConcurrencyVisualizer2019"
+
+# Metrics
+Install-ChocolateyVsixPackage -PackageName "ognjen-babic.code-maintainibility" # https://docs.microsoft.com/en-us/archive/blogs/codeanalysis/maintainability-index-range-and-meaning

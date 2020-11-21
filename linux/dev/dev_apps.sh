484ca094315e1b1c5e90ad8495aaf43aa13637ed
Edinei Cavalcanti <edinei.esc@gmail.com>
2020-11-21 00:09:40 -0300

    linux: Add yarn

--- linux/dev/dev_apps.sh
+++ linux/dev/dev_apps.sh
@@ -2,10 +2,18 @@
 sudo apt install git -y
 sudo apt install gitk -y
 
-sudo apt-get install vim -y
+## Editor
+sudo apt-get install neovim -y
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 sudo snap install code --classic
 
 ## Node
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install nodejs -y
 sudo apt install npm -y
+
+### Yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt update && sudo apt install yarn -y

a80fe60b009924a2ae182dc2bf090afcc2790fc3
Edinei Cavalcanti <edinei.esc@gmail.com>
2020-12-23 15:09:55 -0300

    Rename folders of agilecontent and Remove unuse files

--- windows/aliases.ps1
+++
@@ -1,8 +0,0 @@
-#$ENV:CMDER_ROOT\config\profile.d\*.ps1
-function my-work-dir { cd D:\Dev }
-function my-ip { curl 'https://api.ipify.org?format=json' }
-function my-choco-upgrade-all { choco upgrade all }
-function my-choco-outdated { choco outdated }
-function my-choco-list-local { choco list -l }
-function my-perfmon-report { perfmon /report }
-# curl wttr.in

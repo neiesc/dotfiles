4af6794ce3544348a3d4443e947a44abaec3e095
Edinei Cavalcanti <edinei.esc@gmail.com>
2021-10-29 13:23:21 -0300

    change profile Powersheel

--- windows/dev/terminal/.bash_profile
+++ windows/dev/terminal/.bash_profile
@@ -4,7 +4,6 @@ alias vim="nvim"
 alias vi="nvim"
 alias oldvim="vim"
 
-if [ -f ~/.bash_profile_gvp ]; then . ~/.bash_profile_gvp; fi
 if [ -f ~/.bash_profile_my ]; then . ~/.bash_profile_my; fi
 
 source /c/Dev/bash-wakatime/bash-wakatime.sh
--- windows/dev/terminal/Microsoft.PowerShell_profile.ps1
+++ windows/dev/terminal/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,12 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-PoshPrompt -Theme powerline
+Import-Module -Name Terminal-Icons
+oh-my-posh --init --shell pwsh --config ~/Dropbox/ohmyposhv3-2.json | Invoke-Expression
+Import-Module -Name Posh-Git
 Clear-Host
 Write-Host "edinei.dev"
-function my-work-dir {cd C:\Dev}
+
+function work-dir {cd C:\Dev}
+function work-dir-way2 {cd C:\Dev\Way2\pim}
+
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {

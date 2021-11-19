eb52a0f6e44af04784c02c578bd5b58f89e612c7
Edinei Cavalcanti <edinei.esc@gmail.com>
2021-11-19 13:28:55 -0300

    Add VS extesions

--- windows/dev/vs2022_extensions.ps1
+++ windows/dev/vs2022_extensions.ps1
@@ -4,10 +4,14 @@ if (Test-Path($ChocolateyProfile)) {
 }
 
 # Code style and debug
+#Install-ChocolateyVsixPackage -PackageName "ognjen-babic.code-maintainibility"
 #Install-ChocolateyVsixPackage -PackageName "Diagnostics.DiagnosticsConcurrencyVisualizer2019"
 #Install-ChocolateyVsixPackage -PackageName "MattLaceyLtd.ConstVisualizer"
+#Install-ChocolateyVsixPackage -PackageName "WakaTime.WakaTime"
 Install-ChocolateyVsixPackage -PackageName "JaredParMSFT.VsVim2022Preview"
 Install-ChocolateyVsixPackage -PackageName "SemihOkur.AsyncFixer2022"
+Install-ChocolateyVsixPackage -PackageName "MikeWard-AnnArbor.VSColorOutput"
+Install-ChocolateyVsixPackage -PackageName "VisualStudioPlatformTeam.ProductivityPowerPack2022"
 
 
 # Metrics

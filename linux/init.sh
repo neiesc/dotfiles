69487e90f7fed957fe02a3e98758489e3e6f956e
Edinei Cavalcanti <hi@edinei.dev>
2021-11-28 13:43:56 -0300

    fix

--- linux/init.sh
+++ linux/init.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 ## Update System
-sudo pacman -Syyu
+#sudo pacman -Syyu
 
-sh ./common_apps.sh
-sh ./sysadmin_apps.sh
-sh ./dev_apps.sh
-sh ./creative_apps.sh
+#sh ./common_apps.sh
+#sh ./sysadmin_apps.sh
+#sh ./dev_apps.sh
+#sh ./creative_apps.sh
 # sh ./game_apps.sh
 
 ## Python
@@ -15,14 +15,12 @@ sh ./creative_apps.sh
 # sudo pip3 install -r .requirements.txt
 
 ## link to $HOME
-export DOTFILES_FOLDER=$HOME/Dev/dotfiles
+DOTFILES_FOLDER=$HOME/Dev/dotfiles
 DROPBBOX_FOLDER=$HOME/Dropbox
 
-if [ -d $DROPBBOX_FOLDER ]; then
-    echo "Passou"
-   [ ! -L $HOME/.zsh_history ] && ln -sf $DROPBBOX_FOLDER/history/zsh_history $HOME/.zsh_history
-fi
+ln -sf $DROPBBOX_FOLDER/dotfiles/.zsh_history $HOME/.zsh_history
 
-ln -sf $DOTFILES_FOLDER/common/.gitconfig $HOME/.gitconfig
+ln -sf $DROPBBOX_FOLDER/dotfiles/.gitconfig $HOME/.gitconfig
 ln -sf $DOTFILES_FOLDER/common/.gitignore_global $HOME/.gitignore_global
+
 ln -sf $DOTFILES_FOLDER/common/.zshrc $HOME/.zshrc

88bb11d2fd7d3599387a4fe0fafb677423e3228e
Edinei Cavalcanti <edinei.esc@gmail.com>
2022-01-18 17:20:00 -0300

    windows (cli): add more cli for pim

--- windows/dev/terminal/Microsoft.PowerShell_profile.ps1
+++ windows/dev/terminal/Microsoft.PowerShell_profile.ps1
@@ -5,8 +5,10 @@ Clear-Host
 Write-Host "edinei.dev"
 
 function Work-Dir {cd C:\Dev}
-function Work-Dir-Way2 {cd C:\Dev\Way2\pim}
-function Work-Dir-Way2-Sln {C:\Dev\Way2\pim\Way2Pim.sln}
+function Work-Dir-Way2-PIM {cd C:\Dev\Way2\pim}
+function Work-Dir-Way2-PIM-Sln {C:\Dev\Way2\pim\Way2Pim.sln}
+function Work-Dir-Way2-PIM-TS-Execute {C:\Dev\Way2\pim\Way2Pim.Tasks.Service\bin\Debug\Way2Pim.Tasks.Service.exe}
+function Work-Dir-Way2-PIM-Migration-Execute {C:\Dev\Way2\pim\Way2Pim.Migrations\bin\Debug\Way2Pim.Migrations.exe}
 function gi {git init}
 function ga {git add}
 function gc {git commit -m}

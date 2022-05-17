0284a763bfab10018d1a1c265c58fdcb6aaa3546
Edinei Cavalcanti <edinei.cavalcanti@iverra.com.br>
2022-05-17 13:36:05 -0300

    add httpie

--- linux/common_apps.sh
+++ linux/common_apps.sh
@@ -2,6 +2,8 @@
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+yay httpie
+
 sudo pacman -S --noconfirm keepass
 sudo pacman -S --noconfirm xdotool # keeppass2 need for Auto-Type
 

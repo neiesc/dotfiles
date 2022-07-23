0e993f71d568eccd5be7f644c25e4c0cec740537
Edinei Cavalcanti <hi@edinei.dev>
2022-07-22 23:24:47 -0300

    (linux): add flameshot and peek
    https://dev.to/mauro_codes/switching-to-arch-linux-1hm:

--- linux/common_apps.sh
+++ linux/common_apps.sh
@@ -2,6 +2,9 @@
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+yay -S --noconfirm flameshot
+yay -S --noconfirm peek
+
 yay httpie
 
 sudo pacman -S --noconfirm keepass

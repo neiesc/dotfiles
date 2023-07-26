0062099bdfc9d8faa5ada016a3a4798b9742c7b8
Edinei Cavalcanti <hi@edinei.dev>
2023-07-26 11:07:50 -0300

    add cargo update, yay

--- linux/.config/fish/config.fish
+++ linux/.config/fish/config.fish
@@ -38,9 +38,13 @@ if status is-interactive
 
     # package manager
     alias my-package-update='sudo pacman -Syyu'
+    alias my-package-update-yay='yay'
+    alias my-package-update-cargo='cargo install-update -a'
     alias my-package-list-local='sudo pacman -Q'
     alias my-package-outdated='pacman -Qu'
     alias my-package-orphans-list='pacman -Qdt'
+
+    alias tealdeer='tldr'
 
     # network
     alias my-ip='curl "https://api.ipify.org?format=json"'
@@ -49,6 +53,7 @@ if status is-interactive
     # other
     source /opt/asdf-vm/asdf.fish
     fish_add_path -m ~/.local/bin
+    #/home/edinei/.cargo/bin
 
     thefuck --alias | source
 
--- linux/sysadmin_apps.sh
+++ linux/sysadmin_apps.sh
@@ -1,3 +1,6 @@
 sudo pacman -S --noconfirm tealdeer
 sudo pacman -S --noconfirm bat
 sudo pacman -S --noconfirm dust
+yay -S --noconfirm latencytop
+
+cargo install cargo-update

f6ae4175ae947a576419c8e509695dad64493e26
Edinei Cavalcanti <hi@edinei.dev>
2023-07-27 21:57:41 -0300

    add sync gitkraken theme Dracula

--- dependences_sync.sh
+++ dependences_sync.sh
@@ -5,4 +5,5 @@ DROPBBOX_FOLDER=$HOME/Insync/edinei3a@hotmail.com/Dropbox
 
 cp $DOTFILES_FOLDER/dependences/archcraft-openbox/files/rc.xml $DOTFILES_FOLDER/linux/.config/openbox/rc.xml
 cp $DOTFILES_FOLDER/dependences/archcraft-openbox/files/scripts/ob-furminal $DOTFILES_FOLDER/linux/.config/openbox/scripts/ob-furminal
-cp $DOTFILES_FOLDER/dependences/themes/openbox/Dracula-withoutBorder $HOME/.themes
+cp -r $DOTFILES_FOLDER/dependences/themes/openbox/Dracula-withoutBorder $HOME/.themes
+cp $DOTFILES_FOLDER/dependences/themes/gitkraken/dracula-theme.jsonc $HOME/.gitkraken/themes

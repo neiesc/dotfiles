0dd232fe0fce82a7bdf6298397de99a2d1a2980f
Edinei <hi@edinei.dev>
2023-08-10 17:10:49 -0300

    ajusts and add pligins fisher

--- linux/sysadmin_apps.sh
+++ linux/sysadmin_apps.sh
@@ -1,11 +1,17 @@
-# Terminal
-sudo pacman -S --noconfirm fish
-sudo pacman -S --noconfirm fisher
-fisher update
-
-sudo pacman -S --noconfirm tealdeer
+sudo pacman -S --noconfirm fzf
+sudo pacman -S --noconfirm fd
 sudo pacman -S --noconfirm bat
+sudo pacman -S --noconfirm tealdeer
 sudo pacman -S --noconfirm dust
 yay -S --noconfirm latencytop
 
 cargo install cargo-update
+
+# Terminal
+sudo pacman -S --noconfirm fish
+sudo pacman -S --noconfirm fisher
+
+fisher install ilancosman/tide@v5
+fisher install PatrickF1/fzf.fish
+fisher install ik11235/wakatime.fish
+fisher install dracula/fish

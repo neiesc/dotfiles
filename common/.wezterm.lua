9715d5711040ab9f405b29cebf3f4f984d515631
Edinei Cavalcanti <hi@edinei.dev>
2025-04-20 17:34:25 -0300

    remove 1.0 version

--- common/.wezterm.lua
+++
@@ -1,9 +0,0 @@
-local wezterm = require 'wezterm'
-local config = {}
-
-config.font = wezterm.font 'JetBrains Mono'
-config.color_scheme = 'AdventureTime'
-config.window_background_opacity = 0.88
-config.default_prog = { 'C:\\Users\\Hokage\\AppData\\Local\\Programs\\nu\\bin\\nu.exe', '-l' }
-
-return config

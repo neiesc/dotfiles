7100eb641dd3657ffea096fd3048a2404456fe47
Edinei Cavalcanti <edinei.cavalcanti@allrisk.com.br>
2025-04-23 11:41:40 -0300

    add powershell

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -4,6 +4,6 @@ local config = {}
 config.font = wezterm.font 'JetBrains Mono'
 config.color_scheme = 'AdventureTime'
 config.window_background_opacity = 0.88
-config.default_prog = { 'C:\\Users\\Hokage\\AppData\\Local\\Programs\\nu\\bin\\nu.exe', '-l' }
+config.default_prog = { 'nu', '-l' }
 
 return config

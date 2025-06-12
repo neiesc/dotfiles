5cfefe8dcff65e0bc074644c04edc004e3e7237f
Edinei Cavalcanti <hi@edinei.dev>
2025-06-11 21:02:22 -0300

    Fix some bug and remove default nu in wezterm

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -4,6 +4,5 @@ local config = {}
 config.font = wezterm.font 'JetBrains Mono'
 config.color_scheme = 'AdventureTime'
 config.window_background_opacity = 0.88
-config.default_prog = { 'nu', '-l' }
 
 return config

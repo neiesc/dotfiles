1a9f735c3b0c107a6f6572ef4851eef31c822ca1
Edinei Cavalcanti <hi@edinei.dev>
2025-07-21 11:37:29 -0300

    update

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 local config = {}
 
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font("JetBrains Mono", {weight="Regular", stretch="Normal", style="Normal"})
 config.color_scheme = 'AdventureTime'
 config.window_background_opacity = 0.88
 

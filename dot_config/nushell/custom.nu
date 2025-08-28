2bd33a1edf909958e8152079c04721a663acb68a
Edinei Cavalcanti <hi@edinei.dev>
2025-08-28 14:33:31 -0300

    new release with HyDE

--- dot_config/nushell/custom.nu
+++
@@ -1,5 +0,0 @@
-$env.config.show_banner = false
-
-source ~/.oh-my-posh.nu
-source ./alias.nu
-source ./env.nu
--- dot_wezterm.lua
+++
@@ -1,8 +0,0 @@
-local wezterm = require 'wezterm'
-local config = {}
-
-config.font = wezterm.font("JetBrains Mono", {weight="Regular", stretch="Normal", style="Normal"})
-config.color_scheme = 'AdventureTime'
-config.window_background_opacity = 0.88
-
-return config

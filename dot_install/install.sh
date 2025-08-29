6ba515f2de7cc368a7a8521b9a5538add8ac4412
Edinei Cavalcanti <hi@edinei.dev>
2025-08-29 11:06:56 -0300

    add webapp-launch

--- dot_install/install.sh
+++ dot_install/install.sh
@@ -1,21 +1,25 @@
 echo ### Update system Arch Linux ###
 
-sudo pacman -Syyu
+#sudo pacman -Syyu
+
+echo ### Fix permisions ###
+
+chmod +x bin/*
 
 echo ### Installing chyyezmoi (dotfiles manager) ###
 
-sudo pacman -S chezmoi
+#sudo pacman -S chezmoi
 
 echo ### Installing nvim (LazyVim) ###
 
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
+#git clone https://github.com/LazyVim/starter ~/.config/nvim
+#rm -rf ~/.config/nvim/.git
 
 echo ### Installing HyDE ###
 
-pacman -S --needed git base-devel
-git clone --depth 1 https://github.com/HyDE-Project/HyDE ~/HyDE
+#pacman -S --needed git base-devel
+#git clone --depth 1 https://github.com/HyDE-Project/HyDE ~/HyDE
 
-cp pkg_user.lst ~/HyDE/Scripts
-cd ~/HyDE/Scripts
-./install.sh pkg_extra.lst pkg_user.lst
+#cp pkg_user.lst ~/HyDE/Scripts
+#cd ~/HyDE/Scripts
+#./install.sh pkg_extra.lst pkg_user.lst

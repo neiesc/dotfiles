1c41c574c46c2fed2ca837c2586a1022f072b116
Edinei Cavalcanti <hi@edinei.dev>
2025-08-29 20:50:08 -0300

    fix install #2

--- dot_install/executable_install-system.sh
+++ dot_install/executable_install-system.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-echo "### Update system Arch Linux ###"
-
-#sudo pacman -Syyu
-
-echo "### Fix permisions ###"
-
-#chmod +x ~/.install/bin/*
-
-echo "### Installing chyyezmoi (dotfiles manager) ###"
-
-#sudo pacman -S chezmoi
-
 echo "### Installing nvim (LazyVim) ###"
 
 #git clone https://github.com/LazyVim/starter ~/.config/nvim
@@ -25,3 +13,18 @@ echo "### Installing HyDE ###"
 #cp pkg_user.lst ~/HyDE/Scripts
 #cd ~/HyDE/Scripts
 #./install.sh pkg_extra.lst pkg_user.lst
+
+echo "### Installing Webapp ###"
+
+webapp-install "WhatsApp" https://web.whatsapp.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/whatsapp.png
+webapp-install "Google Photos" https://photos.google.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-photos.png
+webapp-install "Google Contacts" https://contacts.google.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-contacts.png
+webapp-install "Google Messages" https://messages.google.com/web/conversations https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-messages.png
+webapp-install "Grok" https://grok.com https://grok.com/images/favicon-light.png
+webapp-install "ChatGPT" https://chatgpt.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/chatgpt.png
+webapp-install "YouTube" https://youtube.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/youtube.png
+webapp-install "GitHub" https://github.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
+webapp-install "X" https://x.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/x-light.png
+webapp-install "Figma" https://figma.com https://www.veryicon.com/download/png/application/app-icon-7/figma-1?s=256
+webapp-install "Discord" https://discord.com/channels/@me https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/discord.png
+webapp-install "Zoom" https://app.zoom.us/wc/home https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/zoom.png

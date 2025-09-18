703d1b41d0f9f73dfeb98811d862d5a2f4d603d4
Edinei Cavalcanti <hi@edinei.dev>
2025-09-18 20:21:38 -0300

    set JetBrains Toolbox scripts

--- dot_oh-my-zsh/custom/env.zsh
+++ dot_oh-my-zsh/custom/env.zsh
@@ -1,3 +1,6 @@
 export PATH=~/.install/bin:$PATH
+
 export DOTNET_ROOT=$HOME/.dotnet
 export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
+
+export PATH=$PATH:$HOME/.local/share/JetBrains/Toolbox/scripts

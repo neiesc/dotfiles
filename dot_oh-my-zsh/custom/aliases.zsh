eb691ce81ce750396e1595987526cc6fff86d7ca
Edinei Cavalcanti <hi@edinei.dev>
2025-09-18 20:49:28 -0300

    add some alias eza, git etc... and add pkg tk

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -3,12 +3,23 @@ alias g="git"
 alias gs="git status"
 alias gc="git commit -m"
 
-# zen
-alias breath='zenta now --quick'
-alias breathe='zenta now'
-alias reflect='zenta reflect'
+alias gk='gitk'
+alias gg='git gui'
 
 # vim
 alias v='nvim'
 alias vi='nvim'
 alias vim='nvim'
+
+alias ls='eza --icons'
+alias l='ls -lh'
+alias ll='ls -lah'
+alias la='ls -la'
+alias lm='ls -m'
+alias lr='ls -R'
+alias lg='ls -l --group-directories-first'
+
+# zen
+alias breath='zenta now --quick'
+alias breathe='zenta now'
+alias reflect='zenta reflect'

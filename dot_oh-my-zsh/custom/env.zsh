a090a23816cb9a33820f8106065da52666f8a010
Edinei Cavalcanti <hi@edinei.dev>
2025-10-03 08:11:00 -0300

    add proto path in env and add waybar user-style.css

--- dot_oh-my-zsh/custom/env.zsh
+++ dot_oh-my-zsh/custom/env.zsh
@@ -4,4 +4,4 @@ export DOTNET_ROOT=$HOME/.dotnet
 export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
 
 export PATH=$PATH:$HOME/.local/share/JetBrains/Toolbox/scripts
-export PATH=$PATH:$HOME/.local/share/proto/bin/proto
+export PATH=$HOME/.local/share/proto/bin/proto:$PATH

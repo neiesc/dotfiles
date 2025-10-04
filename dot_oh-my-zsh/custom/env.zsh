daa8891295804f882f69f1438135432265eb74cf
Edinei Cavalcanti <hi@edinei.dev>
2025-10-04 12:49:25 -0300

    remove asdf

--- dot_oh-my-zsh/custom/env.zsh
+++ dot_oh-my-zsh/custom/env.zsh
@@ -4,4 +4,3 @@ export DOTNET_ROOT=$HOME/.dotnet
 export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
 
 export PATH=$PATH:$HOME/.local/share/JetBrains/Toolbox/scripts
-export PATH=$HOME/.local/share/proto/bin/proto:$PATH

194a76a7b133abd9743af13b53998db8263cd897
Edinei Cavalcanti <hi@edinei.dev>
2025-11-06 12:32:39 -0300

    mise

--- dot_oh-my-zsh/custom/env.zsh
+++ dot_oh-my-zsh/custom/env.zsh
@@ -6,3 +6,4 @@ export DOTNET_ROOT=$HOME/.dotnet
 export PATH=$PATH:$DOTNET_ROOT:$DOTNET_ROOT/tools
 
 export PATH=$PATH:$HOME/.local/share/JetBrains/Toolbox/scripts
+export PATH=$HOME/.local/share/mise/shims:$PATH

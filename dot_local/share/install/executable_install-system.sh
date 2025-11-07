3e26fc04dc45abd4a44ba05bc65f6f57dd478f40
Edinei Cavalcanti <hi@edinei.dev>
2025-11-07 08:20:05 -0300

    add fix key

--- dot_local/share/install/executable_install-system.sh
+++ dot_local/share/install/executable_install-system.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "##### Install Oh My Zsh #####"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo "### Installing nvim (LazyVim) ###"
 

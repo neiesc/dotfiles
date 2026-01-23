e8f664850831469f97c0570907cab8e2bd5ce7af
Edinei Cavalcanti <hi@edinei.dev>
2026-01-23 20:29:38 -0300

    v2

--- dot_local/share/install/pkg/executable_install-webapp-pessoal.sh
+++ dot_local/share/install/pkg/executable_install-webapp-pessoal.sh
@@ -1,7 +1,6 @@
+echo "🌎 Instalando webapps..."
+
 # Comunication
-webapp-install "Gmail" https://mail.google.com https://ssl.gstatic.com/images/branding/product/1x/hh_gmail_512dp.png
-webapp-install "Tuta Mail" https://app.tuta.com https://app.tuta.com/images/logo-favicon-192.png
-webapp-install "WhatsApp" https://web.whatsapp.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/whatsapp.png
 webapp-install "Telegram" https://web.telegram.org https://web.telegram.org/a/favicon.svg
 webapp-install "Google Messages" https://messages.google.com/web/conversations https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-messages.png
 webapp-install "Google Translate" https://translate.google.com.br https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-translate.png
@@ -10,29 +9,9 @@ webapp-install "Google Translate" https://translate.google.com.br https://cdn.js
 webapp-install "Google Photos" https://photos.google.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-photos.png
 webapp-install "Google Contacts" https://contacts.google.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-contacts.png
 
-# AI
-webapp-install "Google Gemini" https://gemini.google.com https://www.gstatic.com/lamda/images/gemini_sparkle_4g_512_lt_f94943af3be039176192d.png
-webapp-install "Google NotebookLM" https://notebooklm.google.com https://notebooklm.google.com/_/static/branding/v5/dark_mode/favicon/favicon.svg
-webapp-install "ChatGPT" https://chatgpt.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/chatgpt.png
-webapp-install "Grok" https://grok.com https://grok.com/images/favicon-light.png
-webapp-install "Perplexity" https://perplexity.ai https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/perplexity-book-dark.png
-
 # Book & Audibook
 webapp-install "Goodreads" https://goodreads.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/goodreads.png
 webapp-install "Audible" https://audible.com.br https://images-na.ssl-images-amazon.com/images/G/01/audiblemobile/store/image/favicons/icons32px.png
-
-# Dev
-webapp-install "GitHub" https://github.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/github-light.png
-webapp-install "Figma" https://figma.com https://www.veryicon.com/download/png/application/app-icon-7/figma-1?s=256
-webapp-install "ToDiagram" https://todiagram.com/editor https://todiagram.com/logo-48x48.png
-webapp-install "Hacker News" https://news.ycombinator.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/hacker-news.png
-webapp-install "Reddit" https://reddit.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/reddit.png
-webapp-install "StackEdit" https://stackedit.io/app https://stackedit.io/icons-c75a9472175cc17394ba6428d867fbcf/favicon-32x32.png
-
-webapp-install "Pluralsight" https://app.pluralsight.com https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/pluralsight.png
-webapp-install "Domestika" https://domestika.org https://www.domestika.org/favicon-32x32.png
-webapp-install "Stack Overflow" https://stackoverflow.com https://cdn.sstatic.net/Sites/stackoverflow/Img/apple-touch-icon.png?v=c78bd457575a
-webapp-install "DeepLearning.AI" https://deeplearning.ai https://www.deeplearning.ai/static/favicons/favicon.ico
 
 # Social network
 webapp-install "Phanpy" https://phanpy.social https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/phanpy.png
---
+++ dot_local/share/install/pkg/executable_install-webapp-work.sh
@@ -0,0 +1,1 @@
+echo "🌎 Instalando webapps..."

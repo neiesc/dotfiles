d534d9f82f00c6c3a9e54c50548f36d26e9f25c1
Edinei Cavalcanti <hi@edinei.dev>
2026-03-29 18:51:16 +0000

    New (#25)
    * Init v0.4.0
    
    * install.sh
    
    * fix merge
    
    * ajusts
    
    * add fish config
    
    * ajusts
    
    * ajsuts script pkg
    
    * Update personal package list and post-install setup
    
    - Remove obsidian and obs-studio from
      private_dot_local/share/neiesc/package/pkg_pessoal.lst
    - Enable webapp installer by uncommenting
      ./package/install-webapp-pessoal.sh in run_after_install.sh
    - Add Flatpak installs for OpenBoard and OBS Studio in
      run_after_install.sh
    
    * Add external installer and dms doctor to setup
    
    * add tailscale

--- private_dot_local/share/neiesc/run_after_install.sh
+++ private_dot_local/share/neiesc/run_after_install.sh
@@ -2,18 +2,19 @@
 set -eufo pipefail
 
 echo "📦 Instalando pacotes comuns..."
+curl -fsSL https://install.danklinux.com | sh
+dms doctor
 install_pkg package/pkg_common.lst
 
 echo "🏠 Instalando pacotes pessoais..."
 install_pkg.sh package/pkg_pessoal.lst
-#./package/install-webapp-pessoal.sh
+./package/install-webapp-pessoal.sh
+flatpak install flathub ch.openboard.OpenBoard
+flatpak install flathub com.obsproject.Studio
 
 # echo "### Change shell ###"
 
 # chsh -s /usr/bin/zsh
-
-# echo "### Installing Flatpak ###"
-# flatpak install flathub ch.openboard.OpenBoard
 
 # echo "### Set helium default web browser ###"
 #xdg-settings set default-web-browser helium.desktop

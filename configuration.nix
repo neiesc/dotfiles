af9ebf6b5abd42ef857d5915d816accc8aabe12e
Edinei Cavalcanti <hi@edinei.dev>
2026-09-19 07:09:45 -0300

    ajusts

--- configuration.nix
+++ configuration.nix
@@ -28,7 +28,7 @@
   services.gnome.gnome-keyring.enable = true;
   security.pam.services.greetd.enableGnomeKeyring = true;
 
-  programs.noctalia-greeter = {
+  services.displayManager.noctalia-greeter = {
     enable = true;
 
     settings = {
